24287ad9bea8cb81711730293d451e777e6a93da
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-15 16:16:24 +0800

    Complete pseudocode

--- decryptor.py
+++ decryptor.py
@@ -8,3 +8,8 @@ StrOutput=""
 # If letter is #, change to i
 # If letter is +, change to o
 # If letter is !, change to u
+# End loop
+# Execute the code
+
+# String to decrypt: th& q!#ck br+wn f+x j!mps +v&r th& l*zy d+g.
+# expected text: the quick brown fox jumps over the lazy dog.
