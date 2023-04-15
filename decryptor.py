51edc06a8b4f0893bd9fe06e91e5167171690769
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-15 16:21:56 +0800

    making * change to a work, added print for testing.

--- decryptor.py
+++ decryptor.py
@@ -3,13 +3,18 @@
 StrInput=str(input("What is your string?\n:"))
 StrOutput=""
 # Check each of the letters input using loop
-# If letter is *, change to a
+for i in range(len(StrInput)):
+    # If letter is *, change to a
+    if StrInput[i] == "*":
+        StrOutput += "a"
+
 # If letter is &, change to e
 # If letter is #, change to i
 # If letter is +, change to o
 # If letter is !, change to u
 # End loop
 # Execute the code
+print(StrOutput)
 
 # String to decrypt: th& q!#ck br+wn f+x j!mps +v&r th& l*zy d+g.
 # expected text: the quick brown fox jumps over the lazy dog.
