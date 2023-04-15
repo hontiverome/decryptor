57ff4254614d5e5edab69534d582c6c6c878fafe
Jerome Andrei O. Hontiveros <129578499+hontiverome@users.noreply.github.com>
2023-04-15 16:15:34 +0800

    more pseudocode rules

--- decryptor.py
+++ decryptor.py
@@ -3,3 +3,8 @@
 StrInput=str(input("What is your string?\n:"))
 StrOutput=""
 # Check each of the letters input using loop
+# If letter is *, change to a
+# If letter is &, change to e
+# If letter is #, change to i
+# If letter is +, change to o
+# If letter is !, change to u
